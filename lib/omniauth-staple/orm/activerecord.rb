af7c5c1d2e53ac65b59f6247c2f29b9de99de915
acherednikov <alex.cherednikov23@gmail.com>
2017-11-15 12:59:52 +0300

    send encrypted password

--- lib/omniauth-staple/orm/activerecord.rb
+++ lib/omniauth-staple/orm/activerecord.rb
@@ -4,8 +4,8 @@ module OmniauthStaple
   module ActiveRecord
 
 
-    def remote_signup(columns)
-      puts "Remote signup tracked columns: #{columns}"
+    def remote_signup #(columns)
+      puts "Remote signup tracked !"
 
       after_create do
         puts '>>> USER INFO POST >>>'
@@ -16,6 +16,7 @@ module OmniauthStaple
             user: {
               email: self.email,
               encrypted_password: self.encrypted_password,
+              # password: self.encrypted_password,
               first_name: self.first_name,
               last_name: self.last_name
           }
